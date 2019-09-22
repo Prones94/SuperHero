9aae37e5e811fdb12767133c768ac277a4c286a1
Ian Rones <ronesp.85@gmail.com>
2019-09-22 11:39:47 -0700

    [add] attack, add armor, and defend function

--- superheroes.py
+++ superheroes.py
@@ -38,7 +38,7 @@ class Armor:
 
 
 class Hero:
-    def __init__(self, name, abilities, starting_health = 100, current_health = 100):
+    def __init__(self, name, abilities, starting_health = 100):
         ''' 
           Instance properties:
           abilities [list]
@@ -49,27 +49,48 @@ class Hero:
         '''
         self.name = name
         self.starting_health = starting_health
-        self.current_health = current_health
+        self.current_health = starting_health
         self.abilities = []
-        # self.armor = armor
+        self.armors = []
         
     
-    def add_abilities(self, ability):
+    def add_ability(self, ability):
+        '''
+            function adds new abilities to abilities array
+        '''
         self.abilities.append(ability)
 
-        # (Some of these values are passed in above,
-        # others will need to be set at a starting value)
-        # abilities and armors are lists that will contain objects that we can use
-        # pass
+    def attack(self):
+        '''
+            Calculates the total attack of all abilities and returns sum        
+        '''
+        attack_sum = 0
+        for ability in self.abilities:
+            attack_sum += ability.attack()
+        return attack_sum
 
+    def add_armor(self, armor):
+        '''
+            Adds armor to self.armor
+            Armor: Armor Object
+        '''
+        self.armors.append(armor)
+
+    def defend(self, damage_amt):
+        '''
+            Runs `block` method on each armor.
+            Returns sum of all blocks
+        '''
+        block_total = 0
+        for armor in self.armors:
+            block_ed = armor.block()
+            block_total += block_ed
+        return block_total - damage_amt 
 
 if __name__ == '__main__':
-    ability = Ability('Debugging Ability', 50)
-    defense = Armor('Turtle Armor', 50)
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
     hero = Hero("Grace Hopper", 200)
-    hero.add_abilities(ability)
-    print(hero.abilities)
-    print(ability.name)
-    # print(defense.name)
-    print(ability.attack())
-    # print(defense.block())
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
